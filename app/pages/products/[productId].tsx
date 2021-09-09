d365006295852f1bea549e522f17314c59b90b75
Braulio Rodriguez <bdrodriguez.dev@gmail.com>
2021-09-09 17:35:12 -0400

    Basic styling done in products page

--- app/pages/products/[productId].tsx
+++ app/pages/products/[productId].tsx
@@ -18,19 +18,34 @@ export const Product = () => {
 
       <div>
         <h1>Product {product.id}</h1>
-        <h2 className="text-lg font-extrabold leading-tight tracking-tight">
-          Product Feature Requests
-        </h2>
-        {/*<pre>{JSON.stringify(product, null, 2)}</pre>*/}
-        <ul className="space-y-6">
+        <header className="flex flex-row items-center mb-4">
+          <h2 className="text-base uppercase leading-tight tracking-wide font-semibold text-gray-600">
+            Feature Requests
+          </h2>
+          <span className="ml-auto">
+            <Link href={Routes.NewRequestPage()}>
+              <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">
+                New Request
+              </a>
+            </Link>
+          </span>
+        </header>
+        <ul className="space-y-4 p-4 rounded bg-gray-300 m-2">
           {product.requests.map((request) => {
             return (
-              <li className="p-4 rounded-md shadow flex flex-row" key={request.id}>
-                <div className="border">
-                  <button className="">Vote</button>
+              <li
+                className="flex flex-row p-4 rounded shadow bg-[#f8edeb] space-x-4"
+                key={request.id}
+              >
+                <div className="border rounded">
+                  <button className="flex flex-col space-y-4 p-3 rounded shadow-sm hover:bg-blue-200">
+                    <span>123</span>
+                    <span>Vote</span>
+                  </button>
                 </div>
                 <div className="flex flex-col">
                   <span className="text-xl">{request.title}</span>
+                  <span className="text-xl">{request.description}</span>
                 </div>
               </li>
             );
--- app/requests/components/RequestForm.tsx
+++ app/requests/components/RequestForm.tsx
@@ -6,6 +6,8 @@ export { FORM_ERROR } from "app/core/components/Form";
 export function RequestForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
   return (
     <Form<S> {...props}>
+      <LabeledTextField name="title" label="Title" placeholder="Title" />
+      <LabeledTextField name="description" label="Description" placeholder="Description" />
       <LabeledTextField name="name" label="Name" placeholder="Name" />
     </Form>
   );
